6561eecca4227b1bfd5a0e6a013c39004aef1b05
pcm <lamouche444@hotmail.fr>
2020-11-02 16:36:34 +0100

    first fetch

--- index.js
+++ index.js
@@ -2,11 +2,24 @@
 
 var express = require('express');
 var app = express();
+var fetch = require('node-fetch');
+var https = require('https');
 
 const port = process.env.PORT || 3000 ;
 
 app.get("/", function(req, res){
     res.send("helloWorld !");
+})
+
+app.get("/state/:state", function(req, res){
+    let state = req.params.state;
+    let url = "https://api.teleport.org/api/countries/iso_alpha2%3AUS/admin1_divisions/geonames%3A"+state+"/cities/";
+    fetch(url)
+      .then(res => res.json())
+      .then(json => {
+        console.log('fetch', json);
+        res.send('data fetched look your console');
+      });
 })
 
 app.listen(port, function () {
