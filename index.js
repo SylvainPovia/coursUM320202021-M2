cca6e5c9b55349a097c47bc46fe4da10fcc4258f
JojoAkaBrou <joakabrou@gmail.com>
2020-11-05 10:28:50 +0100

    correction du bug clement

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ var https = require('https');
 
 
 var script_state = require('./js/script_state.js');
-var state_id = require('./js/state_id.js');
 var root_state_covid = require('./js/root_state_covid.js');
 var state_list = require('./js/data.js');
 
@@ -34,8 +33,7 @@ app.get("/etat/:etat/ville/:ville", function(req, res){
 
 app.get("/etat/:etat", function(req, res){
     let state = req.params.etat;
-    state_id = state_id.get_state_id(state);
-    var info_state = root_state_covid.root_state_covid(state_id);
+    var info_state = root_state_covid.root_state_covid(state);
     info_state.then((value) => {
       res.format({
         'application/json' : function() {
